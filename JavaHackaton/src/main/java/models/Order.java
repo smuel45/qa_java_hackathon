777020a31968419b6de7c412108f8a8db617745e
smuel45 <sam.cooke97@hotmail.co.uk>
2021-11-26 14:29:05 +0000

    sorting file structure

--- JavaHackaton/src/main/java/models/Order.java
+++ JavaHackaton/src/main/java/models/Order.java
@@ -1,4 +1,24 @@
-package models;
+package classes;
+import java.util.ArrayList;
+import Items;
 
 public class Order {
+    public int cost;
+    ArrayList<Items> items = new ArrayList<Items>();
+
+    public int getCost(){
+        return cost;
+    }
+
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+    public ArrayList<Items> getItems(ArrayList<Items> items){
+        return items;
+    }
+
+    public void ArrayList<Items> addItem(Item item){
+        this.items = this.items.add(item);
+    }
 }
